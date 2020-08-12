d58dd99b009825aa352a8ce2e8b336e3e9cdf744
Benne <Benne.kempenich@googlemail.com>
2020-08-12 09:35:15 +0200

    Meldung: Keine Internetverbindung

--- app/src/main/java/com/example/tankapp/MyReceiver.java
+++ app/src/main/java/com/example/tankapp/MyReceiver.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
     Dialog dialog;
