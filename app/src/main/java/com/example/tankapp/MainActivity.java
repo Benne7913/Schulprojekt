a2aa5550b62e432a3f31b430389e60713561591d
Benne <Benne.kempenich@googlemail.com>
2020-08-13 09:55:28 +0200

    Standort2

--- app/src/main/java/com/example/tankapp/MainActivity.java
+++ app/src/main/java/com/example/tankapp/MainActivity.java
@@ -2,22 +2,40 @@ package com.example.tankapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
+    private static final int PERMISSIONS_FINE_LOCATION = 99 ;
+    //Standort
+    protected LocationManager locationManager;
+    protected String latitude, longitude, altitude;
+
+    //Suche
     AnimationDrawable SearchAnimation;
+
+    //Internetverbindung
     private BroadcastReceiver MyReceiver = null;
 
     @Override
@@ -30,12 +48,38 @@ public class MainActivity extends AppCompatActivity {
         SearchImage.setBackgroundResource(R.drawable.animation);
         SearchAnimation = (AnimationDrawable) SearchImage.getBackground();
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+        else{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+            }
+        }
+
+
         SearchImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SearchAnimation.start();
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch(requestCode){
+            case PERMISSIONS_FINE_LOCATION:
+                if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Keine Berechtigungen um richtig zu arbeiten", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
     }
 
     @Override
@@ -73,5 +117,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         unregisterReceiver(MyReceiver);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        longitude = String.valueOf(location.getLongitude());
+        latitude = String.valueOf(location.getLatitude());
+        if(location.hasAltitude()){
+            altitude = String.valueOf(location.getAltitude());
+        }
+        Toast.makeText(MainActivity.this, longitude + " + " + latitude + " + " + altitude,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
     }
 }
