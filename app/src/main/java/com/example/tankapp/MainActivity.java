3f1303a9ac9e85b69f89529b5544a225d44ce16f
Benne7913 <Benne.kempenich@googlemail.com>
2020-08-17 09:23:45 +0200

    Standort3

--- app/src/main/java/com/example/tankapp/MainActivity.java
+++ app/src/main/java/com/example/tankapp/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.tankapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
@@ -14,11 +17,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     AnimationDrawable SearchAnimation;
     private BroadcastReceiver MyReceiver = null;
+    private GpsTracker gpsTracker;
+    private String latitude;
+    private String longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +37,19 @@ public class MainActivity extends AppCompatActivity {
         SearchImage.setBackgroundResource(R.drawable.animation);
         SearchAnimation = (AnimationDrawable) SearchImage.getBackground();
 
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
         SearchImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SearchAnimation.start();
+                getLocation(view);
             }
         });
     }
@@ -74,4 +90,17 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         unregisterReceiver(MyReceiver);
     }
+
+    public void getLocation(View view){
+        gpsTracker = new GpsTracker(MainActivity.this);
+        if(gpsTracker.canGetLocation()){
+            latitude = String.valueOf(gpsTracker.getLatitude());
+            longitude = String.valueOf(gpsTracker.getLongitude());
+            Toast.makeText(MainActivity.this, latitude + " + " + longitude,
+                    Toast.LENGTH_LONG).show();
+        }else{
+            gpsTracker.showSettingsAlert();
+        }
+    }
+
 }
