0ad5904d22b8d59bd274449ab9fe325c5c467a33
Benne7913 <Benne.kempenich@googlemail.com>
2020-09-07 10:50:17 +0200

    Einen Listener eingebaut für den Android "Zurück-Button" abzufangen

--- app/src/main/java/com/example/tankapp/MyReceiver.java
+++ app/src/main/java/com/example/tankapp/MyReceiver.java
@@ -3,8 +3,10 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +19,17 @@ public class MyReceiver extends BroadcastReceiver {
         dialog = new Dialog(context,R.style.Widget_AppCompat_ActionBar);
         dialog.setContentView(R.layout.internet_connection_check);
         Button restartapp = (Button)dialog.findViewById(R.id.restartapp);
+
+        dialog.setOnKeyListener(new Dialog.OnKeyListener() {
+            @Override
+            public boolean onKey(DialogInterface dialog1, int keyCode, KeyEvent event) {
+
+                if(keyCode == KeyEvent.KEYCODE_BACK){
+                    dialog.show();
+                }
+                return true;
+            }
+        });
 
         restartapp.setOnClickListener(new View.OnClickListener() {
             @Override
